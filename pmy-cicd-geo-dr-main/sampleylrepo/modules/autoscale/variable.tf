06c8b0a5ab2dda50a612922d0324a639941e315b
infrontleong <yukleong.chow@infrontconsulting.com.my>
2025-08-12 16:21:57 +0800

    Delete pmy-cicd-geo-dr-main directory

--- pmy-cicd-geo-dr-main/sampleylrepo/modules/autoscale/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "name" {}
-variable "location" {}
-variable "resource_group_name" {}
-variable "target_resource_id" {}
-variable "tags" {
-  type = map(string)
-}
--- pmy-cicd-geo-dr-main/sampleylrepo/modules/vmss/output.tf
+++
@@ -1,3 +0,0 @@
-output "id" {
-  value = azurerm_linux_virtual_machine_scale_set.vmss001.id
-}
